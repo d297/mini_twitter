7ea2653583a8747bfd36aeec6ab401262b11bf6c
Dmitriy <dmitriy.kostenko94@gmail.com>
2022-01-01 23:32:54 +0700

    add filter

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -11,11 +11,13 @@ const DivHeader = styled.div`
     }
   }
 `;
-const AppHeader = () => {
+const AppHeader = ({ liked, allPosts }) => {
   return (
     <div className="app-header d-flex">
       <h1>Kostenko Dmitriy</h1>
-      <h2>5 записей из них понравилось 0</h2>
+      <h2>
+        {allPosts} записей из них понравилось {liked}
+      </h2>
     </div>
   );
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,21 +18,31 @@ export default class App extends Component {
           label: "Going to learn React",
           important: true,
           id: 1,
+          like: false,
         },
         {
           label: "JS is cool programming language",
           important: false,
           id: 2,
+          like: false,
         },
         {
           label: "New Year",
           important: false,
           id: 3,
+          like: false,
         },
       ],
+      term: "",
+      filter: "all",
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
+    this.repeatCode = this.repeatCode.bind(this);
+    this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
     this.maxId = 4;
   }
 
@@ -60,15 +70,85 @@ export default class App extends Component {
     });
   }
 
+  repeatCode(param, id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const old = data[index];
+      let newItem = {};
+
+      if (param === "important") {
+        newItem = { ...old, important: !old.important };
+      } else {
+        newItem = { ...old, like: !old.like };
+      }
+
+      const newData = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+      return {
+        data: newData,
+      };
+    });
+  }
+
+  onToggleImportant(id) {
+    this.repeatCode("important", id);
+  }
+
+  onToggleLiked(id) {
+    this.repeatCode("liked", id);
+  }
+
+  searchPost(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.label.indexOf(term) > -1;
+    });
+  }
+  onUpdateSearch(term) {
+    this.setState({
+      term,
+    });
+  }
+  filterPost(items, filter) {
+    if (filter === "like") {
+      return items.filter((item) => item.like);
+    } else {
+      return items;
+    }
+  }
+  onFilterSelect(filter) {
+    console.log("!");
+    this.setState({ filter });
+  }
+
   render() {
+    const { data, term, filter } = this.state;
+    const liked = data.filter((item) => item.like).length;
+    const allPosts = data.length;
+
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
+
     return (
       <div className="app">
-        <AppHeader />
+        <AppHeader liked={liked} allPosts={allPosts} />
         <div className="search-panel d-flex">
-          <SearchPanel />
-          <PostStatusFilter />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <PostStatusFilter
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
-        <PostList posts={this.state.data} onDelete={this.deleteItem} />
+        <PostList
+          posts={visiblePosts}
+          onDelete={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleLiked={this.onToggleLiked}
+        />
         <PostAddForm onAdd={this.addItem} />
       </div>
     );
